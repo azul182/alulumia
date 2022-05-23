2854d134897877239cbf6c09e0e76411f9f0bf07
azul182 <84513231+azul182@users.noreply.github.com>
2022-05-23 10:08:28 -0300

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,17 @@ module.exports = (app)=>{
     })
 
     //abrir a view atividades 
-    app.post ('/login',(req,res)=>{
+    app.post ('/login', async(req,res)=>{
+        //conectar com o banco de dados 
+        const conexao = require('../config/database')()
+        //importar a model usuarios 
+        const usuarios = require('../models/usuarios')
+        //procurar pelo endereço de email
+        var procurar = await usuarios.findOne({email:req.body.email})
+        if(!procurar){
+            return send('Email não cadastrado!!')
+        }
+        
         res.render('atividades.ejs')
     })
 }
